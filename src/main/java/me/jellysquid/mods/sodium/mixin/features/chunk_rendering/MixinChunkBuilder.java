e6074221f7e24eed846715caa514794b5178a5e3
Asek3 <64613894+Asek3@users.noreply.github.com>
2024-01-11 01:31:42 +0200

    very fucked up fix for MixinChunkBuilder

--- src/main/java/me/jellysquid/mods/sodium/mixin/features/chunk_rendering/MixinChunkBuilder.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/features/chunk_rendering/MixinChunkBuilder.java
@@ -1,15 +1,28 @@
 package me.jellysquid.mods.sodium.mixin.features.chunk_rendering;
 
+import com.google.common.collect.Queues;
 import net.minecraft.client.renderer.chunk.ChunkRenderDispatcher;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.concurrent.ArrayBlockingQueue;
 
 @Mixin(ChunkRenderDispatcher.class)
 public class MixinChunkBuilder {
-    @ModifyArg(method = "<init>(I)V", index = 0, at = @At("HEAD"))
-    private static int modifyThreadPoolSize(int prev) {
+
+    @Shadow
+    @Final
+    @Mutable
+    private int countRenderBuilders;
+
+    @Redirect(method = "<init>(I)V", at = @At(value = "INVOKE", target = "Lcom/google/common/collect/Queues;newArrayBlockingQueue(I)Ljava/util/concurrent/ArrayBlockingQueue;"))
+    public ArrayBlockingQueue<?> modifyThreadPoolSize(int capacity) {
         // Do not allow any resources to be allocated
-        return 0;
+        this.countRenderBuilders = 0;
+        return Queues.newArrayBlockingQueue(1);
     }
 }
