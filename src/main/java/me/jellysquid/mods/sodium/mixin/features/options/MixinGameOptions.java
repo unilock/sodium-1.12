bdf31105e933487993a8b4d6a4e688594dbdff6c
Asek3 <64613894+Asek3@users.noreply.github.com>
2024-01-13 13:20:49 +0200

    Implemented Cloud option

--- src/main/java/me/jellysquid/mods/sodium/mixin/features/options/MixinGameOptions.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/features/options/MixinGameOptions.java
@@ -1,11 +1,7 @@
-/*package me.jellysquid.mods.sodium.mixin.features.options;
+package me.jellysquid.mods.sodium.mixin.features.options;
 
 import me.jellysquid.mods.sodium.client.SodiumClientMod;
 import me.jellysquid.mods.sodium.client.gui.SodiumGameOptions;
-import net.minecraft.client.option.CloudRenderMode;
-import net.minecraft.client.option.GameOptions;
-import net.minecraft.client.option.GraphicsMode;
-
 import net.minecraft.client.settings.GameSettings;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
@@ -14,19 +10,23 @@ import org.spongepowered.asm.mixin.Shadow;
 @Mixin(GameSettings.class)
 public class MixinGameOptions {
     @Shadow
-    public int viewDistance;
+    public int renderDistanceChunks;
 
     @Shadow
-    public GraphicsMode graphicsMode;
+    public boolean fancyGraphics;
 
+    /**
+     * @author Asek3
+     * @reason Implemented cloud rendering option
+     */
     @Overwrite
-    public CloudRenderMode getCloudRenderMode() {
+    public int shouldRenderClouds() {
         SodiumGameOptions options = SodiumClientMod.options();
 
-        if (this.viewDistance < 4 || !options.quality.enableClouds) {
-            return CloudRenderMode.OFF;
+        if (this.renderDistanceChunks < 4 || !options.quality.enableClouds) {
+            return 0;
         }
 
-        return options.quality.cloudQuality.isFancy(this.graphicsMode) ? CloudRenderMode.FANCY : CloudRenderMode.FAST;
+        return options.quality.cloudQuality.isFancy(this.fancyGraphics) ? 2 : 1;
     }
-}*/
+}
