441746a36b69337f4838da76c5b5edd90a65bc0e
embeddedt <42941056+embeddedt@users.noreply.github.com>
2024-01-12 20:15:54 -0500

    Simplify leaves mixin
    1.12 doesn't have the bug where leaves render their insides on Fast
    graphics, so only need to apply our setting override

--- src/main/java/me/jellysquid/mods/sodium/mixin/features/render_layer/leaves/MixinLeavesBlock.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/features/render_layer/leaves/MixinLeavesBlock.java
@@ -13,28 +13,17 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(BlockLeaves.class)
 public class MixinLeavesBlock extends Block {
-
-    @Shadow
-    protected boolean leavesFancy;
-
     public MixinLeavesBlock(Material blockMaterialIn, MapColor blockMapColorIn) {
         super(blockMaterialIn, blockMapColorIn);
     }
 
-    @Override
-    public BlockRenderLayer getRenderLayer() {
-        return SodiumClientMod.options().quality.leavesQuality.isFancy(leavesFancy) ? BlockRenderLayer.CUTOUT_MIPPED : BlockRenderLayer.SOLID;
-    }
-
-    @Override
-    public boolean shouldSideBeRendered(IBlockState blockState, IBlockAccess blockAccess, BlockPos pos, EnumFacing side) {
-        if (SodiumClientMod.options().quality.leavesQuality.isFancy(Minecraft.getMinecraft().gameSettings.fancyGraphics)) {
-            return super.shouldSideBeRendered(blockState, blockAccess, pos, side);
-        } else {
-            return blockAccess.getBlockState(pos.offset(side)).getBlock() instanceof BlockLeaves || super.shouldSideBeRendered(blockState, blockAccess, pos, side);
-        }
+    @ModifyVariable(method = "setGraphicsLevel", at = @At("HEAD"), argsOnly = true, index = 1)
+    private boolean getSodiumLeavesQuality(boolean fancy) {
+        return SodiumClientMod.options().quality.leavesQuality.isFancy(fancy);
     }
 }
