a1aa1940a1b49896e75ade3faa4cfa496a489d51
embeddedt <42941056+embeddedt@users.noreply.github.com>
2024-01-23 21:29:14 -0500

    Get water & lava textures from internal BlockFluidRenderer fluids
    Fixes Aqua Acrobatics compatibility

--- src/main/java/org/embeddedt/embeddium/render/fluid/EmbeddiumFluidSpriteCache.java
+++ src/main/java/org/embeddedt/embeddium/render/fluid/EmbeddiumFluidSpriteCache.java
@@ -1,15 +1,34 @@
 package org.embeddedt.embeddium.render.fluid;
 
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
+import me.jellysquid.mods.sodium.mixin.features.chunk_rendering.AccessorBlockFluidRenderer;
+import me.jellysquid.mods.sodium.mixin.features.chunk_rendering.AccessorBlockRenderDispatcher;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.BlockFluidRenderer;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidRegistry;
 
 public class EmbeddiumFluidSpriteCache {
     // Cache the sprites array to avoid reallocating it on every call
     private final TextureAtlasSprite[] sprites = new TextureAtlasSprite[3];
     private final Object2ObjectOpenHashMap<ResourceLocation, TextureAtlasSprite> spriteCache = new Object2ObjectOpenHashMap<>();
+    private final TextureAtlasSprite[] waterOverride, lavaOverride;
+
+    public EmbeddiumFluidSpriteCache() {
+        AccessorBlockFluidRenderer fluidRenderer = (AccessorBlockFluidRenderer)((AccessorBlockRenderDispatcher)Minecraft.getMinecraft().getBlockRendererDispatcher()).getFluidRenderer();
+        waterOverride = new TextureAtlasSprite[3];
+        TextureAtlasSprite[] water = fluidRenderer.getAtlasSpritesWater();
+        waterOverride[0] = water[0];
+        waterOverride[1] = water[1];
+        waterOverride[2] = fluidRenderer.getAtlasSpriteWaterOverlay();
+        // Construct array with 3 elements, vanilla only has 2
+        lavaOverride = new TextureAtlasSprite[3];
+        TextureAtlasSprite[] lava = fluidRenderer.getAtlasSpritesLava();
+        lavaOverride[0] = lava[0];
+        lavaOverride[1] = lava[1];
+    }
 
     private TextureAtlasSprite getTexture(ResourceLocation identifier) {
         TextureAtlasSprite sprite = spriteCache.get(identifier);
@@ -23,6 +42,11 @@ public class EmbeddiumFluidSpriteCache {
     }
 
     public TextureAtlasSprite[] getSprites(Fluid fluid) {
+        if (fluid == FluidRegistry.WATER) {
+            return waterOverride;
+        } else if (fluid == FluidRegistry.LAVA) {
+            return lavaOverride;
+        }
         sprites[0] = getTexture(fluid.getStill());
         sprites[1] = getTexture(fluid.getFlowing());
         ResourceLocation overlay = fluid.getOverlay();
