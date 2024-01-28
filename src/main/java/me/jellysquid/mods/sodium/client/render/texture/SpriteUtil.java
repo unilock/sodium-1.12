37615732a1f29bbf7084ccf3eed737a5a549e27c
embeddedt <42941056+embeddedt@users.noreply.github.com>
2024-01-28 15:17:52 -0500

    Remove texture tracking code and hook into CensoredASM for it instead

--- src/main/java/me/jellysquid/mods/sodium/client/render/texture/SpriteUtil.java
+++ src/main/java/me/jellysquid/mods/sodium/client/render/texture/SpriteUtil.java
@@ -1,11 +1,14 @@
 package me.jellysquid.mods.sodium.client.render.texture;
 
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraftforge.fml.common.Loader;
+import zone.rong.loliasm.client.sprite.ondemand.IAnimatedSpriteActivator;
 
 public class SpriteUtil {
+    private static final boolean USING_CENSORED_ASM = Loader.isModLoaded("loliasm");
     public static void markSpriteActive(TextureAtlasSprite sprite) {
-        if (sprite instanceof SpriteExtended) {
-            ((SpriteExtended) sprite).markActive();
+        if (USING_CENSORED_ASM && sprite instanceof IAnimatedSpriteActivator) {
+            ((IAnimatedSpriteActivator) sprite).setActive(true);
         }
     }
 }
