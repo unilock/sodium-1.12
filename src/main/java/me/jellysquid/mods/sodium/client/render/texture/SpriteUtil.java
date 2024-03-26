c383b16acfd44eb0d81e10c9628803b074849a99
Asek3 <64613894+Asek3@users.noreply.github.com>
2024-03-26 14:44:13 +0200

    Added compatibility with FermiumASM

--- src/main/java/me/jellysquid/mods/sodium/client/render/texture/SpriteUtil.java
+++ src/main/java/me/jellysquid/mods/sodium/client/render/texture/SpriteUtil.java
@@ -6,9 +6,12 @@ import zone.rong.loliasm.client.sprite.ondemand.IAnimatedSpriteActivator;
 
 public class SpriteUtil {
     private static final boolean USING_CENSORED_ASM = Loader.isModLoaded("loliasm");
+    private static final boolean USING_FERMIUM_ASM = Loader.isModLoaded("normalasm");
     public static void markSpriteActive(TextureAtlasSprite sprite) {
         if (USING_CENSORED_ASM && sprite instanceof IAnimatedSpriteActivator) {
             ((IAnimatedSpriteActivator) sprite).setActive(true);
+        } else if (USING_FERMIUM_ASM && sprite instanceof mirror.normalasm.client.sprite.ondemand.IAnimatedSpriteActivator) {
+            ((mirror.normalasm.client.sprite.ondemand.IAnimatedSpriteActivator) sprite).setActive(true);
         }
     }
 }
