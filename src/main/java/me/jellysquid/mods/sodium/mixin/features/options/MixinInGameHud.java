fb6a4d318b69764adb6b95b0b2f35dee1c5c62cf
ZZZank <3410764033@qq.com>
2024-05-20 11:58:13 +0800

    more dev note

--- src/main/java/me/jellysquid/mods/sodium/mixin/features/options/MixinInGameHud.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/features/options/MixinInGameHud.java
@@ -9,13 +9,16 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 /**
  * note that both {@link net.minecraft.client.gui.GuiIngame} and {@link net.minecraftforge.client.GuiIngameForge}
- * implements client GUI rendering, and mixins for GuiIngame can be totally useless since GuiIngame might be intentionally
- * ignored and barely invoked
+ * implements client GUI rendering, and mixins for GuiIngame can be totally useless since GuiIngame might be
+ * intentionally ignored and barely invoked
  */
 @Mixin(GuiIngameForge.class)
 public class MixinInGameHud {
     @Redirect(method = "renderGameOverlay", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Minecraft;isFancyGraphicsEnabled()Z"))
     private boolean vintagium$redirectVignette() {
+        //mixin target is `if (renderVignette && Minecraft.isFancyGraphicsEnabled())`
+        //we assume that `renderVignette` is always true, because vanilla/forge will not change its value, and mods that
+        //will change it explicitly must have a reason.
         return SodiumClientMod.options().quality.enableVignette;
     }
 }
